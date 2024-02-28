84388cec7cb060e64048bcc51d3bf351a9f1c389
Semir Zilić <semir.zilic@lutrijabih.ba>
2024-02-28 09:25:02 +0100

    Nalozi za finansije- Povrat u skladiste

--- Materijalno.ViewModel/GlavniViewModel.cs
+++ Materijalno.ViewModel/GlavniViewModel.cs
@@ -19,6 +19,7 @@ namespace Materijalno.ViewModel
         public ICommand SifarnikSkladistaCommand { get; set; }
         public ICommand GlavniCommand { get; set; }
         public ICommand ZaduzenjeCommand { get; set; }
+        public ICommand PovratUSkladisteCommand { get; set; }
 
         public GlavniViewModel(ApplicationViewModel avm)
         {
@@ -33,6 +34,7 @@ namespace Materijalno.ViewModel
         {
             SifarnikSkladistaCommand = new RelayCommand(OtvoriSifarnikSkladista);
             ZaduzenjeCommand = new RelayCommand(OtvoriZaduzenje);
+            PovratUSkladisteCommand = new RelayCommand(OtvoriPovratUSkladiste);
         }
 
         public void OtvoriSifarnikSkladista()
@@ -42,6 +44,11 @@ namespace Materijalno.ViewModel
         public void OtvoriZaduzenje()
         {
             
+        }
+
+        public void OtvoriPovratUSkladiste()
+        {
+            OdabraniVM = new PovratUSkladisteViewModel(this);
         }
 
         public void OtvoriGlavni()
