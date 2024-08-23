e23737771574ef18c56cfc0554252b8ef4de7c0b
eldar.bajrovic <eldar.bajrovic@gmail.com>
2024-08-23 09:42:09 +0200

    Migracija tabele Mat, using Scaffolding

--- Materijalno.Model/EntityModels/materijalno_knjigovodstvoContext.cs
+++ Materijalno.Model/EntityModels/materijalno_knjigovodstvoContext.cs
@@ -23,6 +23,7 @@ namespace Materijalno.Model.EntityModels
         public virtual DbSet<SifarnikMaterijalSkladisteKonto> SifarnikMaterijalSkladisteKonto { get; set; }
         public virtual DbSet<SifarnikMaterijala> SifarnikMaterijala { get; set; }
         public virtual DbSet<SifarnikSkladista> SifarnikSkladista { get; set; }
+        public virtual DbSet<Mat> Mat { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
@@ -105,6 +106,130 @@ namespace Materijalno.Model.EntityModels
                 entity.Property(e => e.PozBrD).HasColumnName("poz_br_d");
 
                 entity.Property(e => e.ZiroRacun).HasColumnName("ziro_racun");
+            });
+
+            modelBuilder.Entity<Mat>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.ToTable("mat");
+
+                entity.Property(e => e.Analst)
+                    .HasColumnName("analst")
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Brdok)
+                    .HasColumnName("brdok")
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Brfak)
+                    .HasColumnName("brfak")
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Brnar)
+                    .HasColumnName("brnar")
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Cartro)
+                    .HasColumnName("cartro")
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Datnar)
+                    .HasColumnName("datnar")
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Datun)
+                    .HasColumnName("datun")
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Fcj)
+                    .HasColumnName("fcj")
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Fvrijed)
+                    .HasColumnName("fvrijed")
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Ident).HasColumnName("ident");
+
+                entity.Property(e => e.Kljnaz).HasColumnName("kljnaz");
+
+                entity.Property(e => e.Kljnaz1).HasColumnName("kljnaz1");
+
+                entity.Property(e => e.Kolic).HasColumnName("kolic");
+
+                entity.Property(e => e.Konto1).HasColumnName("konto1");
+
+                entity.Property(e => e.Konto2)
+                    .HasColumnName("konto2")
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Kontosklad).HasColumnName("kontosklad");
+
+                entity.Property(e => e.Kontosklad1).HasColumnName("kontosklad1");
+
+                entity.Property(e => e.Medus)
+                    .HasColumnName("medus")
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Mjtst).HasColumnName("mjtst");
+
+                entity.Property(e => e.Nc)
+                    .HasColumnName("nc")
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Ourst).HasColumnName("ourst");
+
+                entity.Property(e => e.Porppp)
+                    .HasColumnName("porppp")
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Ppp)
+                    .HasColumnName("ppp")
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Redbr).HasColumnName("redbr");
+
+                entity.Property(e => e.Status)
+                    .HasColumnName("status")
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Tarifa)
+                    .HasColumnName("tarifa")
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Troskovi)
+                    .HasColumnName("troskovi")
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Trospe).HasColumnName("trospe");
+
+                entity.Property(e => e.Vrijed)
+                    .HasColumnName("vrijed")
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Zavtro)
+                    .HasColumnName("zavtro")
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
             });
 
             OnModelCreatingPartial(modelBuilder);
