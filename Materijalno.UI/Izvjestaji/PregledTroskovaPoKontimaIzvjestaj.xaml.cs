3d6a9cc8469de9a63155a81be59c43e163727879
Semir Zilić <semir.zilic@lutrijabih.ba>
2025-02-21 13:50:29 +0100

    test

--- Materijalno.UI/Izvjestaji/PregledTroskovaPoKontimaIzvjestaj.xaml.cs
+++ Materijalno.UI/Izvjestaji/PregledTroskovaPoKontimaIzvjestaj.xaml.cs
@@ -57,6 +57,7 @@ namespace Materijalno.UI.Izvjestaji
                      .GroupBy(row => new { row.Kljnaz, row.Konto1 }) // Group by BOTH Kljnaz and Ident
                      .Select(grouped => new Mat
                      {
+
                          Konto1 = grouped.Key.Konto1, // Konto1 remains the same
                          Kljnaz = grouped.Key.Kljnaz, // Include Kljnaz dynamically
                          Datun = pregledTroskovaPoKontimavm.CurrentItemMat.Datun, // Maintain filtering date
