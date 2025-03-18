7b9cbd1d652cf900fa1bc2cdcdc09412990fb7dd
Semir Zilić <semir.zilic@lutrijabih.ba>
2025-03-18 09:23:12 +0100

    stampanje inventurnih listica

--- Materijalno.UI/StampanjeInventurnihListica.xaml.cs
+++ Materijalno.UI/StampanjeInventurnihListica.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Materijalno.UI.Izvjestaji;
+using Materijalno.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Printing;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -24,5 +27,29 @@ namespace Materijalno.UI
         {
             InitializeComponent();
         }
+        private void TextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                MoveFocusToNextControl(sender as TextBox);
+                e.Handled = true;
+            }
+        }
+
+        private void MoveFocusToNextControl(TextBox currentTextBox)
+        {
+            currentTextBox.MoveFocus(new TraversalRequest(FocusNavigationDirection.Next));
+        }
+
+        private void stampa_button(object sender, RoutedEventArgs e)
+        {
+            if (DataContext is StampanjeInventurnihListicaViewModel stanjeZalihaZaSvaSkladistaViewModel)
+            {
+                StampanjeInventurnihListicaIzvjestaj stampanjeInventurnihListicaIzvjestaj = new StampanjeInventurnihListicaIzvjestaj(stanjeZalihaZaSvaSkladistaViewModel);
+
+                stampanjeInventurnihListicaIzvjestaj.Show();
+            }
+        }
+
     }
 }
